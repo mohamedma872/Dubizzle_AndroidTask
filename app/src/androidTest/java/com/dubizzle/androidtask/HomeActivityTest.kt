e094e9717215a636f3dd9d27ca42a0bbfdc9bcf2
mohammed Elsdody <31186483+mohamedma872@users.noreply.github.com>
2021-12-28 23:41:45 +0200

    add some test to recycle view in Home Activity

---
+++ app/src/androidTest/java/com/dubizzle/androidtask/HomeActivityTest.kt
@@ -0,0 +1,75 @@
+package com.dubizzle.androidtask
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.dubizzle.androidtask.ui.HomeActivity
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(HomeActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.recyclerView),
+                childAtPosition(
+                    withClassName(`is`("androidx.cardview.widget.CardView")),
+                    0
+                )
+            )
+        )
+        onView(isRoot()).perform(waitFor(5000))
+        recyclerView.perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+    }
+
+    fun waitFor(delay: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $delay milliseconds"
+            override fun perform(uiController: UiController, v: View?) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+        }
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
